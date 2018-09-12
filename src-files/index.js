c0f6176bc6d48c303501d2c157dbbc7323b31261
tphdev <tphdevdrop@gmail.com>
2018-09-12 18:14:32 -0500

    creating readme

---
+++ src-files/index.js
@@ -0,0 +1,16 @@
+import  React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './css/styles.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// a.1 - Import Browser Router
+
+// a.2 - Create `AppRouter` Component wrapper around <App/> using BrowserRouter
+
+
+
+
+// a.3 - Pass instantiated AppRouter component to
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
